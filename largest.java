254b82782c9b3fa6859a9009277786bca37673fc
dhyan4649-collab <dhyan4649@gmail.com>
2026-05-05 11:39:10 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+        int arr[]={50,30,20,10,40};
+        int temp=0;
+        int k=3;
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    
+                }
+            }
+            
+        }
+        
+        System.out.println("the kth largest element is"+arr[arr.length-k]);
+        
+        
+    }
+}
