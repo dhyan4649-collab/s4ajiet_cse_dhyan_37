a89f0990813dd274119988f04dee2ee31498f8c6
dhyan4649-collab <dhyan4649@gmail.com>
2026-05-06 14:45:31 +0530

    Create anagram.java

---
+++ anagram.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class AnagramCheck {
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent"));  
+        System.out.println(isAnagram("evil", "vile"));      
+        System.out.println(isAnagram("hello", "world"));
+    }
+}
